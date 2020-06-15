742339218213225aef2b65f986aee4fe439b1079
Ryan Busby <ryanbusby@mac.local>
2020-06-15 03:35:13 -0600

    rew

--- scroll.py
+++ scroll.py
@@ -10,9 +10,9 @@ def DisplayScrollingLeft(txt, lcd):
   if len(txt) < 17:
     txt = txt + (16 * ' ' )
   while True:
-     lcd.set_cursor(0, 0)
+     lcd.setCursor(0, 0)
      lcd.message(txt[:16])
-     lcd.set_cursor(0, 1)
+     lcd.setCursor(0, 1)
      lcd.message(datetime.now().strftime('%b %d  %H:%M:%S'))
      time.sleep(0.5)
      txt = txt[1:] + txt[0]
