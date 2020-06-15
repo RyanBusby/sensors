2ac78bf4b573b450d1f3c0538a2f733e039a1682
Ryan Busby <ryanbusby@mac.local>
2020-06-15 03:28:52 -0600

    rewrite

--- scroll.py
+++ scroll.py
@@ -1,4 +1,11 @@
-def DisplayScrollingLeft(txt):
+########################################################################
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
+
+from time import sleep, strftime
+from datetime import datetime
+
+def DisplayScrollingLeft(txt, lcd):
   txt = txt.strip() + ' '
   if len(txt) < 17:
     txt = txt + (16 * ' ' )
@@ -10,4 +17,8 @@ def DisplayScrollingLeft(txt):
      time.sleep(0.5)
      txt = txt[1:] + txt[0]
 
-DisplayScrollingLeft('This scrolls to the left.')
+PCF8574_address = 0x27
+mcp = PCF8574_GPIO(PCF8574_address)
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+
+DisplayScrollingLeft('This scrolls to the left.', lcd)
