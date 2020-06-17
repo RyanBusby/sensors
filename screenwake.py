07796c96df387a36386589333641c7e52300dcce
Ryan Busby <ryanbusby@mac.local>
2020-06-17 15:48:30 -0600

    add sonar

--- screenwake.py
+++ screenwake.py
@@ -1,4 +1,74 @@
 import os
+#!/usr/bin/env python3
+########################################################################
+# Filename    : UltrasonicRanging.py
+# Description : Get distance via UltrasonicRanging sensor
+# auther      : www.freenove.com
+# modification: 2019/12/28
+########################################################################
+import RPi.GPIO as GPIO
+import time
+
+trigPin = 16
+echoPin = 18
+MAX_DISTANCE = 220          # define the maximum measuring distance, unit: cm
+timeOut = MAX_DISTANCE*60   # calculate timeout according to the maximum measuring distance
+
+def pulseIn(pin,level,timeOut): # obtain pulse time of a pin under timeOut
+    t0 = time.time()
+    while(GPIO.input(pin) != level):
+        if((time.time() - t0) > timeOut*0.000001):
+            return 0;
+    t0 = time.time()
+    while(GPIO.input(pin) == level):
+        if((time.time() - t0) > timeOut*0.000001):
+            return 0;
+    pulseTime = (time.time() - t0)*1000000
+    return pulseTime
+
+def getSonar():     # get the measurement results of ultrasonic module,with unit: cm
+    GPIO.output(trigPin,GPIO.HIGH)      # make trigPin output 10us HIGH level
+    time.sleep(0.00001)     # 10us
+    GPIO.output(trigPin,GPIO.LOW) # make trigPin output LOW level
+    pingTime = pulseIn(echoPin,GPIO.HIGH,timeOut)   # read plus time of echoPin
+    distance = pingTime * 340.0 / 2.0 / 10000.0     # calculate distance with sound speed 340m/s
+    return distance
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
+    GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
+    GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
+
+def loop():
+    while(True):
+        distance = getSonar() # get distance
+        print ("The distance is : %.2f cm"%(distance))
+        if distance > 70:
+            continue
+        if state()[8] == 'a':
+            continue
+        elif state()[8] == '0':
+            wake()
+        time.sleep(.1)
+
+
+def wake():
+    os.popen("tvservice -p")
+    os.popen("sudo chvt 9 && sudo chvt 7")
+
+def snooze():
+    os.popen("tvservice -o")
+
+def state():
+    os.popen("tvservice -s")
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        GPIO.cleanup()
 
 def wake():
     os.system("tvservice -p")
@@ -8,8 +78,7 @@ def snooze():
     os.system("tvservice -o")
 
 def state():
-    os.system("tvservice -s")
-
+    os.popen("tvservice -s").read()
 
 if __name__ == "__main__":
     # from time import sleep
