373e223b9c562e5f0ebd9bd77c1c1fdb89aec561
Ryan Busby <ryanbusby@mac.local>
2020-06-20 19:03:43 -0600

    add run files

---
+++ dbsetup.py
@@ -0,0 +1,36 @@
+import sys, datetime
+
+from flask import Flask, render_template, request, redirect, jsonify, url_for, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/birdhouse'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Temp(db.Model):
+    __tablename__ = 'temp'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    value = db.Column(db.Float, nullable=False)
+
+class Humid(db.Model):
+    __tablename__ = 'humid'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    value = db.Column(db.Float, nullable=False)
+
+class Infared(db.Model):
+    __tablename__ = 'infared'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    value = db.Column(db.Boolean, nullable=False)
+
+class SolarVolts(db.Model):
+    __tablename__ = 'solarvolts'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    value = db.Column(db.Float, nullable=False)
---
+++ run.py
@@ -0,0 +1,4 @@
+# import all sensor code
+# import database
+
+# run each sensor and push to db
