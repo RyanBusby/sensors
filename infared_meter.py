12d8edbd0a75709f0bdf4169dfabcceebd92a58e
Ryan Busby <ryanbusby@mac.local>
2020-06-22 17:59:36 -0600

    fix obj

---
+++ infared_meter.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+
+class Infared():
+    def __init__(self, ir_pin, led_pin):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(ir_pin, GPIO.IN)
+        GPIO.setup(led_pin, GPIO.OUT)
+
+    def read():
+        if GPIO.input(self.ir_pin) == GPIO.HIGH:
+            GPIO.output(self.led_pin, GPIO.HIGH)
+            return True
+
+        else:
+            GPIO.output(self.led_pin,GPIO.LOW)
+            return False
