b2ae15b709286b737026f2394a174a22906da176
Ryan Busby <ryanbusby@mac.local>
2020-06-22 16:17:10 -0600

    fix ch

--- solar_voltage_meter.py
+++ solar_voltage_meter.py
@@ -4,5 +4,5 @@ class SolarVoltage():
     def __init__(self):
         self.adc = Adafruit_ADS1x15.ADS1115()
 
-    def read(self):
-        return self.adc.read_adc(0, gain=1)
+    def read(self, ch):
+        return self.adc.read_adc(ch, gain=1)
