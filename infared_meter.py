c06ad558246ce52702cb00fe97d6feb509cf23f5
Ryan Busby <ryanbusby@mac.local>
2020-06-22 18:01:39 -0600

    fix objexct

--- infared_meter.py
+++ infared_meter.py
@@ -6,11 +6,11 @@ class Infared():
         GPIO.setup(ir_pin, GPIO.IN)
         GPIO.setup(led_pin, GPIO.OUT)
 
-    def read():
+    def read(self):
         if GPIO.input(self.ir_pin) == GPIO.HIGH:
             GPIO.output(self.led_pin, GPIO.HIGH)
             return True
 
         else:
-            GPIO.output(self.led_pin,GPIO.LOW)
+            GPIO.output(self.led_pin, GPIO.LOW)
             return False
