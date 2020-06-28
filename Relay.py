5acf32e7ce64031c7dee47f8dfc4ed708fbd0c32
Ryan Busby <ryanbusby@mac.local>
2020-06-28 00:22:49 -0600

    change pin

--- Relay.py
+++ Relay.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ########################################################################
 # Filename    : Relay.py
-# Description : Control Relay and Motor via Button 
+# Description : Control Relay and Motor via Button
 # Author      : www.freenove.com
 # modification: 2019/12/27
 ########################################################################
@@ -9,11 +9,11 @@ import RPi.GPIO as GPIO
 import time
 
 relayPin = 11     # define the relayPin
-buttonPin = 12    # define the buttonPin
+buttonPin = 7    # define the buttonPin
 debounceTime = 50
 
-def setup():    
-    GPIO.setmode(GPIO.BOARD)       
+def setup():
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(relayPin, GPIO.OUT)   # set relayPin to OUTPUT mode
     GPIO.setup(buttonPin, GPIO.IN)   # set buttonPin to INTPUT mode
 
@@ -24,7 +24,7 @@ def loop():
     lastButtonState = GPIO.HIGH
     reading = GPIO.HIGH
     while True:
-        reading = GPIO.input(buttonPin)     
+        reading = GPIO.input(buttonPin)
         if reading != lastButtonState :
             lastChangeTime = round(time.time()*1000)
         if ((round(time.time()*1000) - lastChangeTime) > debounceTime):
@@ -41,9 +41,9 @@ def loop():
                     print("Button is released!")
         GPIO.output(relayPin,relayState)
         lastButtonState = reading # lastButtonState store latest state
-    
+
 def destroy():
-    GPIO.cleanup()                      
+    GPIO.cleanup()
 
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
@@ -52,4 +52,3 @@ if __name__ == '__main__':     # Program entrance
         loop()
     except KeyboardInterrupt:   # Press ctrl-c to end the program.
         destroy()
-
